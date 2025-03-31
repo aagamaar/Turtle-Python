d166bdcb8f16dbc1c7e93f6f4e187986293af586
aagamaar <aagamaar01@gmail.com>
2025-03-31 22:36:42 +0530

    added  lines

--- Project 1/proj1.py
+++ Project 1/proj1.py
@@ -1,3 +1,4 @@
+import tkinter as TK
 from turtle import *
 from colorsys import *
 setposition(50,-50)
@@ -9,5 +10,10 @@ h=0
 for i in range(120):
     for i in range(4):
         color(hsv_to_rgb(h, 1, 1))
-        
-
+        h+=0.003
+        circle(40+i*5,90)
+        forward(250)
+        left(90)
+    rt(10)
+    hideturtle()
+done()
